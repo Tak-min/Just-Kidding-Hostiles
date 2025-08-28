c9c40758cedfd07eee94ced77e60de5157e9666e
takumi <taku810616@gmail.com>
2025-08-29 03:14:52 +0900

    update all and making it first step

---
+++ OrpheusInMineCraft/common/src/main/java/com/nstut/simplyscreens/blocks/ScreenBlock.java
@@ -0,0 +1,81 @@
+package com.nstut.simplyscreens.blocks;
+
+import com.nstut.simplyscreens.client.screens.ImageLoadScreen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.jetbrains.annotations.NotNull;
+import com.nstut.simplyscreens.blocks.entities.ScreenBlockEntity;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ScreenBlock extends Block implements EntityBlock {
+
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.UP, Direction.DOWN);
+    public static final IntegerProperty STATE = IntegerProperty.create("state", 0, 2);
+
+    public static final int STATE_CHILD = 0;
+    public static final int STATE_ANCHOR = 1;
+    public static final int STATE_ERROR = 2;
+
+    public ScreenBlock() {
+        super(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion());
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH)
+                .setValue(STATE, STATE_ANCHOR));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
+        blockStateBuilder.add(FACING, STATE);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(FACING, context.getNearestLookingDirection().getOpposite())
+                .setValue(STATE, STATE_ANCHOR);
+    }
+
+    @Override
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public @NotNull BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
+        return new ScreenBlockEntity(pos, state);
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        if (level.isClientSide) {
+            Minecraft.getInstance().setScreen(new ImageLoadScreen(pos));
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> blockEntityType) {
+        return level.isClientSide ? null : (lvl, pos, blockState, blockEntity) -> {
+            if (blockEntity instanceof ScreenBlockEntity screenBlockEntity) {
+                screenBlockEntity.tick();
+            }
+        };
+    }
+}
